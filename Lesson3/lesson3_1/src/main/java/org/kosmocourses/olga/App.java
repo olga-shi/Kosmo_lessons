417a71ee701fabd26f5cba7e9f01f364a57f2dfe
Olga-Shi <shi.olg@gmail.com>
2018-12-16 23:49:48 +0300

    Add comment for lesson3_1

--- Lesson3/lesson3_1/src/main/java/org/kosmocourses/olga/App.java
+++ Lesson3/lesson3_1/src/main/java/org/kosmocourses/olga/App.java
@@ -16,21 +16,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        int rub = 100;
-        double course = 1.6469;
-        App converter = new App();
+        int rub = 100; // указываем тут сумму в рублях, которую хотим поменять
+        double course = 1.6469; // указываем цену рубля
+        App converter = new App(); // создаем экземпляр класса App
+        /** вызываем метод print у экземпляра класса App (для красивой печати)
+         *  В параметрах метода указываются переменные  rub, course
+         *  В качестве третьего аргумента, вызывается метод toUSD
+        */
         converter.print(rub, course, converter.toUSD(rub, course));
-        rub = 50;
+        rub = 50; // еще раз - для себя
         converter.print(rub, course, converter.toUSD(rub, course));
     }
 
-    public double toUSD(int rub, double course)
+    public double toUSD(int rub, double course) // метод для конвертации
     {
-        double usd = rub * course;
-        return usd;
+        double usd = rub * course; // формула конвертации
+        return usd; // возвращаем результат
     }
 
-    public void print(int rub, double course, double usd)
+    public void print(int rub, double course, double usd) // метод для красивой печати
     {
         System.out.println("За " + rub + " Руб, по курсу " + course + " вы получите " + usd + " $");
     }
